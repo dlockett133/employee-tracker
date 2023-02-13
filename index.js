33f51bf41e8cb65ca175a60ea746ad2207678028
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-12 23:41:05 -0500

    connected to database

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require('console.table');
 
-const db = mysql.createConnection();
-
+const db = mysql.createConnection(
+    {
+    host: 'localhost',
+    user: 'root',
+    password: 'lupin1993',
+    database: 'employee_db'
+    },
+    console.log("connected to the database")
+);
