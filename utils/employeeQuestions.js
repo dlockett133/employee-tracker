45ef3d95115d47a46ed3e94cc8f956b77c91e063
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-13 00:43:10 -0500

    created employeeQuestions

---
+++ utils/employeeQuestions.js
@@ -0,0 +1,17 @@
+const startQuestion = [
+    {
+        type: `list`,
+        name: `queryStart`,
+        message: `Would you like to:`,
+        choices: [
+            `View all departments`,
+            `View all roles`,
+            `View all employees`,
+            `Add a department`, 
+            `Add a role`, 
+            `Add an employee`,
+            `Update an employee role`
+        ]
+    
+    }
+]
